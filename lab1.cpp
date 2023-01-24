2efeafb970b45b0af9fee1a9aefae55f53b29762
Daniel Torres <dtorres@odin.cs.csubak.edu>
2023-01-24 08:54:59 -0800

    First commit

---
+++ lab1.cpp
@@ -0,0 +1,17 @@
+// Daniel Torres
+// Lab-1 Jan 24
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+    // This will be used later 
+    int a = rand() % 10  + 1; 
+    cout << a << endl;
+    
+    return 0;
+}
